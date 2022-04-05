b57974ac7a091925bad36430a24b3e3791b6e6c8
Iliianotapoet <ilya.10189@gmail.com>
2022-04-05 15:27:28 +0300

    Add constructor models

--- src/models/models.js
+++ src/models/models.js
@@ -9,6 +9,78 @@ const User = sequelize.define('user', {
     }
 });
 
+const Lesson = sequelize.define('lesson', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    authorId: {
+        type: DataTypes.UUID,
+        allowNull: false
+    }
+});
+
+const Task = sequelize.define('task', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    }
+});
+
+const Sentence = sequelize.define('sentence', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    text: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    gapPosition: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+});
+
+const Option = sequelize.define('option', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    value: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    isRight: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+User.hasMany(Lesson, {as: "lessons"});
+Lesson.belongsTo(User, {
+    foreignKey: "authorId",
+    as: "author"
+});
+
+Lesson.hasMany(Task);
+Task.belongsTo(Lesson);
+
+Task.hasMany(Sentence);
+Sentence.belongsTo(Task);
+
+Sentence.hasMany(Option);
+Option.belongsTo(Sentence);
+
 module.exports = {
-    User
+    User,
+    Lesson,
+    Task,
+    Sentence,
+    Option
 }
