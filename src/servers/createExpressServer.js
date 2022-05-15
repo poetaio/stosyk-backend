58a948a8218c6d27437c8b211beb96133df1adf7
KostiantynKorzh <kostyantynkorzh@gmail.com>
2022-05-15 22:52:16 +0300

    Убрал проверку токена для storage controller

--- src/servers/createExpressServer.js
+++ src/servers/createExpressServer.js
@@ -21,7 +21,7 @@ module.exports = (pubsub) => {
     // отдельный РЕСТ(!) эндпоинт, который принимает файл, сохраняет его
     // (сейчас с помощью реализации на AWS S3) и выдает public link,
     // который потом сохраняется в attachments для таски
-    app.post('/storage',  upload.single('file'), parseRestRequest, (req, res, next) =>
+    app.post('/storage',  upload.single('file'), (req, res, next) =>
         storageController.uploadFileAndGetLink(req, res));
 
     app.use('/graphql', graphqlHTTP((req, res) => ({
