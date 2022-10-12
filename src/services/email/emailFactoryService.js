37c4f2a76fb6a609c97cc56a85af0ac886fd9b5f
ValeriiaStopen <valeriia.s@code-care.pro>
2022-10-12 17:05:05 +0300

    [Fix] - change email to info@stosyk.app

--- src/services/email/emailFactoryService.js
+++ src/services/email/emailFactoryService.js
@@ -2,7 +2,7 @@ const emailConstants = require('../../utils/constants')
 class EmailFactoryService {
     async createConfirmationEmail(email, verificationCode){
         if(process.env.ENVIRONMENT === "DEV"){
-            email = "stopen.lera@gmail.com"
+            email = "info@stosyk.app"
         }
         const mailOptions = {
             from: {
@@ -18,7 +18,7 @@ class EmailFactoryService {
 
     async createResetPassEmail(email, resetPassCode){
         if(process.env.ENVIRONMENT === "DEV"){
-            email = "stopen.lera@gmail.com"
+            email = "info@stosyk.app"
         }
         const mailOptions = {
             from: {
@@ -34,7 +34,7 @@ class EmailFactoryService {
 
     createInvitationEmail(schoolName, studentEmail, invitationToken) {
         if(process.env.ENVIRONMENT === "DEV"){
-            studentEmail = "stopen.lera@gmail.com"
+            studentEmail = "info@stosyk.app"
         }
         return this.createEmail(
             studentEmail,
